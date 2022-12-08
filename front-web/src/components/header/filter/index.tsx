5a36c3168cde1065dd9867eab8a3047819382292
nevesade <adenasiocarvalho19@gmail.com>
2022-12-08 21:19:13 +0000

    Criando o componente Filtro - Parte 2

--- front-web/src/components/header/filter/index.tsx
+++ front-web/src/components/header/filter/index.tsx
@@ -1,9 +1,36 @@
 import './styles.css';
+import 'flatpickr/dist/themes/material_green.css';
+
+import FlatPicker from 'react-flatpickr';
+import FlatPickerlib from 'flatpickr';
+import { Portuguese } from 'flatpickr/dist/l10n/pt';
+
+FlatPickerlib.localize(Portuguese);
 
 function Filter() {
+  const onChangeDate = (dates: Date[]) => {
+    console.log(dates);
+  };
+
   return (
     <div className="filter-container  base-card">
-      <h1>filter</h1>
+      <FlatPicker
+        className="filter-input"
+        onChange={onChangeDate}
+        placeholder="Selecione um período"
+        options={{
+          mode: 'range',
+          dateFormat: 'd/m/y',
+          showMonths: 2
+        }}
+      />
+
+      <select className="filter-input">
+        <option value="">Selecione um gênero</option>
+        <option value="MALE">Masculino</option>
+        <option value="FEMALE">Feminino</option>
+        <option value="OTHER">Outro</option>
+      </select>
     </div>
   );
 }
