95299dead5224769a522ef4b059711c976998155
nevesade <adenasiocarvalho19@gmail.com>
2022-12-11 03:34:08 +0000

    Criando o componente de Vendas Por Data - Parte 1

---
+++ front-web/src/components/sales-by-date/index.tsx
@@ -0,0 +1,14 @@
+import './styles.css';
+
+function SalesByDate() {
+  return (
+    <div className="sales-by-date-container">
+      <div>
+        <h4 className="sales-by-date-title">Evolução das vendas</h4>
+        <span className="sales-by-date-period">01/01/2017 a 31/01/2017</span>
+      </div>
+    </div>
+  );
+}
+
+export default SalesByDate;
