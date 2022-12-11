575cf9245779a5d4507787d47e4d6250d990d349
nevesade <adenasiocarvalho19@gmail.com>
2022-12-11 04:04:11 +0000

    Criando o componente de Vendas Por Data - Parte 2

--- front-web/src/components/sales-by-date/index.tsx
+++ front-web/src/components/sales-by-date/index.tsx
@@ -2,10 +2,19 @@ import './styles.css';
 
 function SalesByDate() {
   return (
-    <div className="sales-by-date-container">
+    <div className="sales-by-date-container base-card">
       <div>
         <h4 className="sales-by-date-title">Evolução das vendas</h4>
         <span className="sales-by-date-period">01/01/2017 a 31/01/2017</span>
+      </div>
+      <div className="sales-by-date-data">
+        <div className="sales-by-date-quantity-container">
+          <h2 className="sales-by-date-quantity">R$ 464.988,00</h2>
+          <span className="sales-by-date-quantity-label">Vendas no período</span>
+          <span className="sales-by-date-quantity-description">
+            O gráfico mostra as vendas em todas as lojas
+          </span>
+        </div>
       </div>
     </div>
   );
