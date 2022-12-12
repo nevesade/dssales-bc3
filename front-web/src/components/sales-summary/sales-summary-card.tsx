812feda4066b1614a24f12bbb21cdd5a58c29bad
nevesade <adenasiocarvalho19@gmail.com>
2022-12-12 20:49:55 +0000

    Criando o componente de Sumário das Vendas - Parte 2

--- front-web/src/components/sales-summary/sales-summary-card.tsx
+++ front-web/src/components/sales-summary/sales-summary-card.tsx
@@ -1,7 +1,14 @@
 import './styles.css';
+import { ReactComponent as Avatar } from '../../assets/avatar-icon.svg';
 
 function SalesSummaryCard() {
-  return <div className="sales-summary-card base-card"></div>;
+  return (
+    <div className="sales-summary-card base-card">
+      <Avatar />
+      <h3 className="sales-summary-card-value">534.00</h3>
+      <span className="sales-summary-card-label">Média</span>
+    </div>
+  );
 }
 
 export default SalesSummaryCard;
