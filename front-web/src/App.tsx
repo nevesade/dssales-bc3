419d84bc22e772c0c47a508ff3d9322a8525d2dc
nevesade <adenasiocarvalho19@gmail.com>
2023-07-08 13:25:25 +0100

    12-22 Integrando o filtro com o sumário de vendas

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -22,7 +22,7 @@ function App() {
         <Filter onFilterChange={onFilterChange} />
         <SalesByDate filterData={filterData} />
         <div className="sales-overview-container">
-          <SalesSummary />
+          <SalesSummary filterData={filterData} />
           <PieChartCard
             labels={['Uberlândia', 'Ituiutaba', 'Araguari']}
             name={'Lojas'}
--- front-web/src/components/sales-summary/index.tsx
+++ front-web/src/components/sales-summary/index.tsx
@@ -4,14 +4,46 @@ import { ReactComponent as AvatarIcon } from '../../assets/avatar-icon.svg';
 import { ReactComponent as BarIcon } from '../../assets/bar-icon.svg';
 import { ReactComponent as DoneIcon } from '../../assets/done-icon.svg';
 import { ReactComponent as SyncIcon } from '../../assets/sync-icon.svg';
+import { FilterData, SalesSummaryData } from '../../types';
+import { useMemo, useEffect, useState } from 'react';
+import { buildFilterParams, makeRequest } from '../../utils/request';
 
-function SalesSummary() {
+type Props = {
+  filterData?: FilterData;
+};
+
+const initialSummary = {
+  avg: 0,
+  count: 0,
+  max: 0,
+  min: 0
+};
+
+function SalesSummary({ filterData }: Props) {
+  const [summary, setSummary] = useState<SalesSummaryData>(initialSummary);
+
+  const params = useMemo(() => buildFilterParams(filterData), [filterData]);
+
+  useEffect(() => {
+    makeRequest
+      .get<SalesSummaryData>('/sales/summary', { params })
+      .then((response) => {
+        // eslint-disable-next-line prettier/prettier
+        setSummary(response.data);
+        console.log(response.data);
+      })
+      .catch(() => {
+        console.error('Error to fetch sales by date');
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [params]);
+
   return (
     <div className="sales-summary-container">
-      <SalesSummaryCard value={430} label="Média" icon={<DoneIcon />} />
-      <SalesSummaryCard value={730} label="Quatidade" icon={<SyncIcon />} />
-      <SalesSummaryCard value={130} label="Mínima" icon={<BarIcon />} />
-      <SalesSummaryCard value={330} label="Máxima" icon={<AvatarIcon />} />
+      <SalesSummaryCard value={summary?.avg?.toFixed(2)} label="Média" icon={<DoneIcon />} />
+      <SalesSummaryCard value={summary?.count} label="Quatidade" icon={<SyncIcon />} />
+      <SalesSummaryCard value={summary?.min} label="Mínima" icon={<BarIcon />} />
+      <SalesSummaryCard value={summary?.max} label="Máxima" icon={<AvatarIcon />} />
     </div>
   );
 }
